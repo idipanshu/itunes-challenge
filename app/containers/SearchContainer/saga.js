316b0b15aece0e86ca472c77b520bbc5d6d5a3a7
Dipanshu Chaubey <dipanshu@wednesday.is>
2022-05-26 18:36:20 +0530

    Separate saga, reducer, selectors for fetching Track Details

--- app/containers/SearchContainer/saga.js
+++ app/containers/SearchContainer/saga.js
@@ -1,16 +1,33 @@
 import { put, call, takeLatest } from 'redux-saga/effects';
-import { getSongs } from '@services/songApi';
+import { getSongs, getSongDetails } from '@services/songApi';
 import { searchContainerTypes, searchContainerCreators } from './reducer';
 
-const { REQUEST_GET_SONGS } = searchContainerTypes;
-const { successGetSongs, failureGetSongs } = searchContainerCreators;
+const { REQUEST_GET_SONGS, REQUEST_GET_SONG_DETAILS } = searchContainerTypes;
+const { successGetSongs, successGetSongDetails, failureGetSongs } = searchContainerCreators;
 
 export function* getItunesSongs(action) {
   const response = yield call(getSongs, action.searchedTerm);
   const { data, ok } = response;
 
   if (ok) {
-    yield put(successGetSongs(data.results));
+    let result = {};
+
+    data.results.forEach((song) => {
+      result[song.trackId] = { ...song };
+    });
+
+    yield put(successGetSongs(result));
+  } else {
+    yield put(failureGetSongs(data));
+  }
+}
+
+export function* getTrackDetails(action) {
+  const response = yield call(getSongDetails, action.trackId);
+  const { data, ok } = response;
+
+  if (ok) {
+    yield put(successGetSongDetails(data.results[0]));
   } else {
     yield put(failureGetSongs(data));
   }
@@ -18,4 +35,5 @@ export function* getItunesSongs(action) {
 
 export default function* searchContainerSaga() {
   yield takeLatest(REQUEST_GET_SONGS, getItunesSongs);
+  yield takeLatest(REQUEST_GET_SONG_DETAILS, getTrackDetails);
 }
